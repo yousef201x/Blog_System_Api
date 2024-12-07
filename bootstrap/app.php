badb24cc6b4c76badbbb49245bd4da45f132c756
Yousef201x <yousefmohamed2006x@gmail.com>
2024-12-08 01:33:23 +0200

    update bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,17 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Router;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        function (Router $router) {
+            $router->middleware('api')->prefix('api')->group(function () {
+                //
+            });
+        },
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
