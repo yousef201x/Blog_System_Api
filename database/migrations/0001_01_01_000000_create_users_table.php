fc070a77a9c0ac93a3288199dfd825b519451aed
Yousef201x <yousefmohamed2006x@gmail.com>
2024-12-09 23:48:50 +0200

    Update sessions table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -32,8 +32,8 @@ return new class extends Migration
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->longText('payload')->nullable();
+            $table->time('last_activity')->nullable();
         });
     }
 
