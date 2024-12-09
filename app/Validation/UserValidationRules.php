efcd470654201df6aa96e082d2c2fa25ce27698a
Yousef201x <yousefmohamed2006x@gmail.com>
2024-12-09 23:54:25 +0200

    add Custom Validation Rules

---
+++ app/Validation/UserValidationRules.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Validation;
+
+trait UserValidationRules
+{
+    function registerRules(): array
+    {
+        return [
+            // Yousef Mohamed
+            'name' => 'required|string|max:255|regex:/^[a-zA-Z\s]+$/',
+            // yousef@mail.com // yousef_123@mail.com // yousef.m@mail.com
+            'email' => 'required|email|max:255|unique:users,email',
+            // Password@123 // Str0ng!Pass // MySecur3$Pass
+            'password' => 'required|string|min:8|max:64|regex:/[A-Z]/|regex:/[a-z]/|regex:/[0-9]/|regex:/[@$!%*?&#]/',
+        ];
+    }
+}
